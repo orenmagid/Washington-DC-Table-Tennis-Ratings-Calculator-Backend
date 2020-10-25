a4a24f007d0a9e7c042a8e8420688fad4e0ab6ad
Oren Magid <oren@meeter-app.com>
2020-10-25 11:15:37 -0400

    added /api/v1 namespacing to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :ratings
-  resources :player_matches
-  resources :matches
-  resources :sessions
-  resources :player_groups
-  resources :groups
-  resources :players
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :ratings
+      resources :player_matches
+      resources :matches
+      resources :sessions
+      resources :player_groups
+      resources :groups
+      resources :players
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
