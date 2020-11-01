41680e577f4657c82db23c7c9259b4c471ad2f5b
Oren Magid <oren@meeter-app.com>
2020-10-31 22:38:25 -0400

    session create (calculate) operational

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,4 +3,31 @@ class Api::V1::SessionsController < ApplicationController
     @sessions = Session.all
     render json: @sessions, include: ["group", "matches", "matches.players", "matches.players.ratings"]
   end
+
+  def create
+    matches = params[:matches]
+    date = params[:date]
+    # date = Time.new(date)
+    puts("date", date)
+    group_id = params[:group_id]
+
+    session = Session.create(date: date)
+    group = Group.find(group_id)
+    group.sessions.push(session)
+    
+    matches.each do |match|
+      new_match = Match.create_match(match["winner"]["id"], match["loser"]["id"], session.id)
+    end
+
+    session.calculate_ratings
+
+    if session.save
+      render json: session, status: :accepted
+    end
+
+  end
+
+  private
+
+
 end
