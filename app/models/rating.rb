111b394528b3c113bd57ba9b3fde6a4bbeeab064
Oren Magid <oren@meeter-app.com>
2020-11-09 14:42:35 -0500

    removed sort scope for ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,5 @@ class Rating < ApplicationRecord
   belongs_to :player, optional: true
   belongs_to :session, optional: true
 
-  default_scope { order(date: :desc) }
+  # default_scope { order(date: :desc) }
 end
