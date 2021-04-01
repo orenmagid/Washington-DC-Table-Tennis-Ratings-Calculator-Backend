c7bdd8438e8e4121c1b9b60b40ddaee95f05badf
orenmagid <oren.michael.magid@gmail.com>
2021-03-31 20:13:45 -0400

    added method to delete and recalculate all session-related ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,20 @@ class Rating < ApplicationRecord
   belongs_to :session, optional: true
 
   # default_scope { order(date: :desc) }
+
+  def self.recalculate_all
+    Player.all.each do |player|
+      player.update(most_recent_rating: player.ratings.first.value)
+    end
+
+    Session.all.each do |session|
+      session.ratings.each do |rating|
+        rating.destroy
+      end
+    end
+
+    Session.all.each do |session|
+      session.calculate_ratings
+    end
+  end
 end
